fe67ed856828e516cf4cfb23eccb21d458b414c0
Elissa <elissa.matsushita@gmail.com>
2021-07-05 18:30:13 -0700

    remove photo credit

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ function App() {
                 key={window.location.pathname}
               />
             </Switch>
-            <div id="photocredit">photos by <a href="https://www.kaoverii.com/" rel="noopener noreferrer" target="_blank">Kaoverii Silva</a></div>
+            {/* <div id="photocredit">photos by <a href="https://www.kaoverii.com/" rel="noopener noreferrer" target="_blank">Kaoverii Silva</a></div> */}
           </MainContainer>
         </BodyContainer>
       </Router>
